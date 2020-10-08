b55d1b742d7351351c88de5f826a8090a337adec
username <qkrdydals327@gmail.com>
2020-10-08 19:28:19 +0900

    Updates

---
+++ precache-manifest.9c4218d77b6cf3ab3dac57253d325673.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfd39d062a988602d4be1e37e5579b0e",
+    "url": "/DragonSky2357/mini-twitter/index.html"
+  },
+  {
+    "revision": "1174269e7193c80ca66b",
+    "url": "/DragonSky2357/mini-twitter/static/css/main.3a91e2b1.chunk.css"
+  },
+  {
+    "revision": "97a32e2f3af4b8597cf7",
+    "url": "/DragonSky2357/mini-twitter/static/js/2.5a7ce7cd.chunk.js"
+  },
+  {
+    "revision": "8e844923efaee6906e00c8fde9e55716",
+    "url": "/DragonSky2357/mini-twitter/static/js/2.5a7ce7cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1174269e7193c80ca66b",
+    "url": "/DragonSky2357/mini-twitter/static/js/main.6acb24f6.chunk.js"
+  },
+  {
+    "revision": "e8cf92faefbdd48849fe",
+    "url": "/DragonSky2357/mini-twitter/static/js/runtime-main.9e30f0c9.js"
+  }
+]);
